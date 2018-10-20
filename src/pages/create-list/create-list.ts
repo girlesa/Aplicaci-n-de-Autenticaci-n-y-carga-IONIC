48f13089ff83655738dc5f773c329f1d7e625233
jorgeluisvera2004 <jorgeluisvera2004@gmail.com>
2018-10-20 16:59:22 -0500

    aplicación de ionic de crear listas con token hay un servidor de rails por debajo de esto

--- src/pages/create-list/create-list.ts
+++ src/pages/create-list/create-list.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams,ViewController } from 'ionic-angular';
+import { ApiProvider } from '../../providers/api/api';
 
 /**
  * Generated class for the CreateListPage page.
@@ -13,18 +14,35 @@ import { IonicPage, NavController, NavParams,ViewController } from 'ionic-angula
   selector: 'page-create-list',
   templateUrl: 'create-list.html',
 })
+
 export class CreateListPage {
 
+  public name: string;
+  public description: string;
+
   constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              public viewCtrl: ViewController) {
+    public navParams: NavParams,
+    public viewCtrl: ViewController,
+    private api: ApiProvider) {
   }
 
- /* ionViewDidLoad() {
-    console.log('ionViewDidLoad CreateListPage');
-  }*/
-  public dismiss():void{
+  public dismiss(): void {
     this.viewCtrl.dismiss();
   }
 
+  public createList(): void {
+    const params = {
+      name: this.name,
+      description: this.description
+    };
+    this.api.createList(params).subscribe((status: boolean) => {
+      if (status) {
+        alert('lista creada');
+      } else {
+        alert('error');
+      }
+    })
+  }
+
 }
+ 
--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -1,7 +1,7 @@
-import { HttpClient,HttpErrorResponse} from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import {map, catchError} from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
 import { ViewController, NavController, NavParams } from 'ionic-angular';
 
 /*
@@ -14,13 +14,13 @@ import { ViewController, NavController, NavParams } from 'ionic-angular';
 export class ApiProvider {
   //esta url es para acceder a unos recursos que se encuentran en heroku
   //donde están los recursos para consultar ..
-    private url : string;
-      // Crear la variable token
-    private token: string;
+  private url: string;
+  // Crear la variable token
+  private token: string;
 
-    constructor(public http: HttpClient) {
-   // console.log('Hello ApiProvider Provider');
-    this.url='https://fede-todo-list.herokuapp.com/api' ;
+  constructor(public http: HttpClient) {
+    // console.log('Hello ApiProvider Provider');
+    this.url = 'https://fede-todo-list.herokuapp.com/api';
     // Cargar el token cada que inicia el app
     this.loadToken();
 
@@ -31,22 +31,19 @@ export class ApiProvider {
         'Authorization': this.token
       }
     });
-}
-    public isAuth():boolean {
-  return this.token  ?  true: false;
- }
- 
- 
-   public auth(params): Observable<boolean> {
+  }
+  public isAuth(): boolean {
+    return this.token ? true : false;
+  }
+
+
+  public auth(params): Observable<boolean> {
     return this.http.post(`${this.url}/auth`, {
       auth: params
     }).pipe(map((response: any) => {
       // Guardar el token
       this.token = response.jwt;
       this.saveToken();
-      return true;
-
-     
       return true;
     }), catchError((error: HttpErrorResponse) => {
       return Observable.of(false);
@@ -60,47 +57,34 @@ export class ApiProvider {
     }), catchError((error: HttpErrorResponse) => {
       return Observable.of(false);
     }))
-}
-// Crear funciones para guardar y cargar el token de la memoria
-private saveToken(): void {
-  localStorage.setItem('token', this.token);
-}
-
-private loadToken(): void {
-  this.token = localStorage.getItem('token');
-}
-/*public getLists(): Observable<any> {
-  return this.http.get(`${this.url}/v1/list`, {
-    headers: {
-      'Authorization': this.token
-    }
-  });
-}
-*/
-public createList(params): Observable<boolean> {
-  return this.http.post(`${this.url}/v1/list`, {
-    list: params
-  }, {
-    headers: {
-      'Authorization': this.token
-    }
-  }).pipe(map((response: any) => {
-    return true;
-  }), catchError((error: HttpErrorResponse) => {
-    return Observable.of(false);
-  }))
-}
-
-export class CreateListPage {
-
-  public name: string;
-  public description: string;
-
-  constructor(public navCtrl: NavController,
-    public navParams: NavParams,
-    public viewCtrl: ViewController,
-    private api: ApiProvider) {
+  }
+  // Crear funciones para guardar y cargar el token de la memoria
+  private saveToken(): void {
+    localStorage.setItem('token', this.token);
   }
 
-
+  private loadToken(): void {
+    this.token = localStorage.getItem('token');
+  }
+  /*public getLists(): Observable<any> {
+    return this.http.get(`${this.url}/v1/list`, {
+      headers: {
+        'Authorization': this.token
+      }
+    });
+  }
+  */
+  public createList(params): Observable<boolean> {
+    return this.http.post(`${this.url}/v1/list`, {
+      list: params
+    }, {
+        headers: {
+          'Authorization': this.token
+        }
+      }).pipe(map((response: any) => {
+        return true;
+      }), catchError((error: HttpErrorResponse) => {
+        return Observable.of(false);
+      }))
+  }
 }
